704f54d3a0bd59ab598b5b8565ca6ea4869bdd33
Sergey Ocheretenko <ocheretenko.s@gmail.com>
2021-01-11 13:32:30 +0200

    Create anagram.js

---
+++ JavaScript/anagram.js
@@ -0,0 +1,30 @@
+const anagram = function(str1, str2){
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
+  if (str1.length != str2.length){
+    return false;
+  }
+  let object1 = {};
+  let object2 = {};
+  for(let i = 0; i < str1.length; i++){
+    object1[str1[i]] ? object1[str1[i]]++ : object1[str1[i]] = 1
+    object2[str2[i]] ? object2[str2[i]]++ : object2[str2[i]] = 1
+  }
+  let array = [];
+  for(const key in object1){
+    array.push(key in object2 && object1.key === object2.key);
+  }
+  return !(false in array);
+}
+
+let object = {
+  name: 2
+}
+console.log(anagram('finder', 'Friend'));
+console.log(anagram('hello', 'bye'));
+
+object.b = 1
+object.a = 2
+object.c = 3
+console.log(object)
+console.log(Object.keys(object))
